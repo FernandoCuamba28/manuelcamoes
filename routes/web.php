73ebd39ac9e4c2bd80212bb1d854d8e15403da7b
Fernandocuamba <fernandocuamba28@gmail.com>
2021-12-22 01:55:58 +0200

    minhas coisas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-// Route::get('/', function () {
-//     return view('Cv');
-// });
-
-Route::get('/', [ContactController::class,'contact']);
-
-Route::get('/send-email', [ContactController::class,'sendEmail'])->name("contact.send");
-
-Route::post('/enviar', [ContactController::class,'sendEmail'])->name('contact.send');
+Route::get('/', function () {
+    return view('welcome');
+});
